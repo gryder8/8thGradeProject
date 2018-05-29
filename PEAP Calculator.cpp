c7e502ab9ede0131478844f0ff2104dcca2410f2
gryder8 <36829208+gryder8@users.noreply.github.com>
2018-05-29 11:14:10 -0700

    Update PEAP Calculator.cpp

--- PEAP Calculator.cpp
+++ PEAP Calculator.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  Science Project Calculator
 //
-//  Created by Gavin Ryderon 2/23/16.
+//  Created by Gavin Ryder on 2/23/16.
 //  Copyright © 2016 Gavin Ryder. All rights reserved.
 //
 
